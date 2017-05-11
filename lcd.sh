7e5099bf988ff45f9ffde47b3d7392853e28cd9b
Matthew Malensek <matt@malensek.net>
2017-05-11 15:45:19 -0600

    Add info parser function

--- lcd.sh
+++ lcd.sh
@@ -8,17 +8,21 @@ lcd_init() {
     port="${2}"
 
     exec 3<>/dev/tcp/${host}/${port}
+
     if [[ ${?} -eq 0 ]]; then
         echo "hello" >&3
         response=$(lcd_get_response)
-        export LCD_WIDTH=$(lcd_get_info "${response}" "wid")
-        export LCD_HEIGHT=$(lcd_get_info "${response}" "hgt")
-        export LCD_CELL_WIDTH=$(lcd_get_info "${response}" "cellwid")
-        export LCD_CELL_HEIGHT=$(lcd_get_info "${response}" "cellhgt")
+        export LCD_WIDTH=$(lcd_parse_info "${response}" "wid")
+        export LCD_HEIGHT=$(lcd_parse_info "${response}" "hgt")
+        export LCD_CELL_WIDTH=$(lcd_parse_info "${response}" "cellwid")
+        export LCD_CELL_HEIGHT=$(lcd_parse_info "${response}" "cellhgt")
     else
         echo "fail"
         return 1
     fi
 }
 
+lcd_parse_info() {
+    sed "s/.* ${2} \([0-9.]*\).*/\1/g" <<< "${1}"
+}
 
